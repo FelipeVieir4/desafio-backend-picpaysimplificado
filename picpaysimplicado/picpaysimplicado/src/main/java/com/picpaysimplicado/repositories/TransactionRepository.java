404bd910c7144a2f47ce23b7c8a51eb1a09f6065
Felipe Vieira <luizfelipevieira01@gmail.com>
2023-10-08 14:10:20 -0300

    feat: Create transaction repository

---
+++ picpaysimplicado/picpaysimplicado/src/main/java/com/picpaysimplicado/repositories/TransactionRepository.java
@@ -0,0 +1,11 @@
+package com.picpaysimplicado.repositories;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.picpaysimplicado.domain.transaction.Transaction;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+}
