ef93263e0863eac6bec54e944dc2e56593b3cc09
Felipe Vieira <luizfelipevieira01@gmail.com>
2023-10-08 19:52:45 -0300

    feat: Add UserService

---
+++ picpaysimplicado/picpaysimplicado/src/main/java/com/picpaysimplicado/services/UserService.java
@@ -0,0 +1,34 @@
+package com.picpaysimplicado.services;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.picpaysimplicado.domain.user.User;
+import com.picpaysimplicado.domain.user.UserType;
+import com.picpaysimplicado.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+        if (sender.getUserType() == UserType.MERCHANT) {
+            throw new Exception("Usuário do tipo logista não está autorizado a realizar transações");
+        }
+        if (sender.getBalance().compareTo(amount) < 0) {
+            throw new Exception("Saldo insuficiente");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return this.repository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado"));
+    }
+
+    public void saveUser(User user) {
+        this.repository.save(user);
+    }
+}
