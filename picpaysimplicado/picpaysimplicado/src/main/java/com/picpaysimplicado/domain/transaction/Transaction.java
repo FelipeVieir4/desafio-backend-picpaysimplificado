04e6c4f44a37a9acea90cdde47664ba89d9b9a01
Felipe Vieira <luizfelipevieira01@gmail.com>
2023-10-08 13:41:26 -0300

    add User and Transaction model class

---
+++ picpaysimplicado/picpaysimplicado/src/main/java/com/picpaysimplicado/domain/transaction/Transaction.java
@@ -0,0 +1,59 @@
+package com.picpaysimplicado.domain.transaction;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import com.picpaysimplicado.domain.user.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Entidade que representa uma transação no sistema.
+ */
+@Entity(name = "transactions") // Indica que esta classe é uma entidade JPA com o nome "transactions".
+@Table(name = "transactions") // Especifica o nome da tabela correspondente no banco de dados
+                              // ("transactions").
+@Getter // Gera automaticamente métodos de getter para todos os campos da classe.
+@Setter // Gera automaticamente métodos de setter para todos os campos da classe.
+@AllArgsConstructor /*
+                     * Gera automaticamente um construtor com todos os campos da classe como
+                     * argumentos.
+                     */
+@EqualsAndHashCode(of = "id") /*
+                               * Gera métodos equals() e hashCode() usando apenas o campo 'id' para
+                               * comparação.
+                               */
+public class Transaction {
+
+    @Id // Indica que o campo anotado é a chave primária da entidade no banco de dados.
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // usada para especificar a estratégia de geração de valores
+                                                        // para a chave primária
+    private Long id;
+    private BigDecimal amount;
+    @ManyToOne // Estabelece uma relação muitos-para-um com a entidade User.
+    @JoinColumn(name = "sender_id") /*
+                                     * Especifica o nome da coluna na tabela "transactions" que contém
+                                     * a chave estrangeira para o remetente (campo sender da entidade
+                                     * User).
+                                     */
+    private User sender;
+    @ManyToOne // Estabelece uma relação muitos-para-um com a entidade User.
+    @JoinColumn(name = "receiver_id") /*
+                                       * Especifica o nome da coluna na tabela "transactions" que contém
+                                       * a chave estrangeira para o destinatário (campo receiver da
+                                       * entidade User).
+                                       */
+    private User receiver;
+    private LocalDateTime timestamp;
+
+}
---
+++ picpaysimplicado/picpaysimplicado/src/main/java/com/picpaysimplicado/domain/user/User.java
@@ -0,0 +1,45 @@
+package com.picpaysimplicado.domain.user;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Entidade que representa um usuário no sistema.
+ */
+@Entity(name = "users") // Indica que esta classe é uma entidade JPA com o nome "users".
+@Table(name = "users") // Especifica o nome da tabela correspondente no banco de dados ("users").
+@Getter // Gera automaticamente métodos de getter para todos os campos da classe.
+@Setter // Gera automaticamente métodos de setter para todos os campos da classe.
+@AllArgsConstructor /*
+                     * Gera automaticamente um construtor com todos os campos da classe como
+                     * argumentos.
+                     */
+@EqualsAndHashCode(of = "id") /*
+                               * Gera métodos equals() e hashCode() usando apenas o campo 'id' para
+                               * comparação.
+                               */
+public class User {
+
+    @Id // ndica que o campo anotado é a chave primária da entidade no banco de dados.
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // usada para especificar a estratégia de geração de valores
+                                                        // para a chave primária
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true) // Garante que o valor desta coluna no banco de dados seja único.
+    private String document;
+    @Column(unique = true) // Garante que o valor desta coluna no banco de dados seja único.
+    private String email;
+    private String password;
+    private BigDecimal balance;
+    @Enumerated(EnumType.STRING) /*
+                                  * Especifica que o tipo do campo no banco de dados será uma string
+                                  * representando a enumeração.
+                                  */
+    private UserType userType;
+}
